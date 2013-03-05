b06fd5087d88e3d08a8619504ecdb259cc71638e
gumar <gumar@MacBook-Pro-de-gumar.local>
2013-03-05 12:23:47 +0100

    adding filtering possibilities

--- lib/locomotive/liquid/filters/text.rb
+++ lib/locomotive/liquid/filters/text.rb
@@ -8,8 +8,7 @@ module Locomotive
         end
         
         def html_for_json(input)
-          f = input.to_json.html_safe.to_s.gsub('/', '\/').gsub("\r", "")
-          f.html_safe? ? f.html_safe : f
+          input.to_json.html_safe.to_s.gsub('/', '\/').gsub("\r", "")
         end
 
       end
