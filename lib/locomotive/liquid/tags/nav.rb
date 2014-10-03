a0078e0cc84fa49f2086a1f4dd9fecf627772f9f
gumar <gumar@MacBook-Pro-de-gumar.local>
2014-10-03 15:29:11 +0200

    modification for isba menu

--- lib/locomotive/liquid/tags/nav.rb
+++ lib/locomotive/liquid/tags/nav.rb
@@ -45,9 +45,9 @@ module Locomotive
 
           output = children_output.join("\n")
 
-          if @options[:no_wrapper] != 'true'
-            output = %{<ul id="#{@options[:id]}">\n#{output}</ul>}
-          end
+          #if @options[:no_wrapper] != 'true'
+          #  output = %{<ul id="#{@options[:id]}">\n#{output}</ul>}
+          #end
 
           output
         end
@@ -71,16 +71,31 @@ module Locomotive
 
         # Returns a list element, a link to the page and its children
         def render_entry_link(page, css, depth)
+
+          if depth == 1
+            if page.children_with_minimal_attributes.size > 0
+              output = %{<h1>#{page.title}</a></h1>}
+              output << render_entry_children(page, depth.succ) if (depth.succ <= @options[:depth].to_i)
+            else
+              output = %{<h1 id="#{page.slug.dasherize}_  link"><a href="/#{page.fullpath}">#{page.title}</h1>}
+            end
+          else
+            selected = @current_page.slug =~ /^#{page.slug}/ ? 'current' : ''
+            output  = %{<li class="link #{selected} #{css}">}
+            output << %{<a href="/#{page.fullpath}">#{page.title}</a>}
+            output << %{</li>}
+          end
+
           
-          selected = @current_page.slug =~ /^#{page.slug}/ ? ' current' : ''
+          #selected = @current_page.slug =~ /^#{page.slug}/ ? ' current' : ''
 
-          icon = @options[:icon] ? '<span></span>' : ''
-          label = %{#{icon if @options[:icon] != 'after' }#{page.title}#{icon if @options[:icon] == 'after' }}
+          #icon = @options[:icon] ? '<span></span>' : ''
+          #label = %{#{icon if @options[:icon] != 'after' }#{page.title}#{icon if @options[:icon] == 'after' }}
 
-          output  = %{<li id="#{page.slug.dasherize}-link" class="link#{selected} #{css}">}
-          output << %{<a href="/#{page.fullpath}" data-description="#{page.menu_subtitle}">#{label}</a>}
-          output << render_entry_children(page, depth.succ) if (depth.succ <= @options[:depth].to_i)
-          output << %{</li>}
+          #output  = %{<li id="#{page.slug.dasherize}-link" class="link#{selected} #{css}">}
+          #output << %{<a href="/#{page.fullpath}" data-description="#{page.menu_subtitle}">#{label}</a>}
+          #output << render_entry_children(page, depth.succ) if (depth.succ <= @options[:depth].to_i)
+          #output << %{</li>}
 
           output.strip
         end
